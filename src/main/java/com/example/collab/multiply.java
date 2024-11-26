c39c54d858b32f9f34a892f0b654be448bbfb014
Killamsettysrujana14 <killamsettysrujana@gmail.com>
2024-11-26 22:00:25 +0530

    updated

--- src/main/java/com/example/collab/multiply.java
+++ src/main/java/com/example/collab/multiply.java
@@ -1,6 +1,6 @@
 package com.example.collab;
-public class multi {
-    public int multiply(int a,int b)
+public class multiply{
+    public int mult(int a,int b)
     {
            return a*b;
     }
